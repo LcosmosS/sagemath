d1b988bb3825731250f4cadf4d11c5fe4808a4ee
Tobias Diez <code@tobiasdiez.de>
2025-03-07 16:02:50 +0100

    Factory instantiation should yield normal Python objects

--- src/sage/rings/polynomial/pbori/pbori.pyx
+++ src/sage/rings/polynomial/pbori/pbori.pyx
@@ -8101,6 +8101,6 @@ cdef class PolynomialFactory:
             raise TypeError("cannot convert %s to BooleanPolynomial" %
                             type(arg))
 
-cdef MonomialFactory Monomial = MonomialFactory()
-cdef PolynomialFactory Polynomial = PolynomialFactory()
-cdef VariableFactory Variable = VariableFactory()
+Monomial = MonomialFactory()
+Polynomial = PolynomialFactory()
+Variable = VariableFactory()
